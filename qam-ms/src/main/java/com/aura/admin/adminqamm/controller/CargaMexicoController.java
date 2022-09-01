59f2f561409b14b294e3601134c0b7511d0a7514
EDGAR\EdgarAd <edgaragustin748@gmail.com>
2022-09-01 11:01:11 -0500

    creación de clase para recibir layout

--- qam-ms/src/main/java/com/aura/admin/adminqamm/controller/CargaMexicoController.java
+++ qam-ms/src/main/java/com/aura/admin/adminqamm/controller/CargaMexicoController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.aura.admin.adminqamm.dto.request.CargaRequestDto;
 import com.aura.admin.adminqamm.exception.BusinessException;
 import com.aura.admin.adminqamm.service.CargaMexicoService;
 
@@ -28,15 +30,16 @@ public class CargaMexicoController {
 	Logger logger = LoggerFactory.getLogger(CargaMexicoController.class);
 	
 	@PostMapping("")
-    public ResponseEntity<Object> insertCargaMexico(@RequestAttribute("username") int loggedIdUser, @ModelAttribute MultipartFile cargaMexico){
+    public ResponseEntity<Object> insertCargaMexico(@RequestAttribute("username") int loggedIdUser, @ModelAttribute CargaRequestDto cargaMexico){
         try {
+        	logger.info(cargaMexico.getArchivo().getName());
         	cargaMexicoService.insertCargaMexico(loggedIdUser, cargaMexico);
             return new ResponseEntity<Object>("", HttpStatus.OK);
         }catch (BusinessException e){
             return new ResponseEntity<Object>("{\"error\":\""+e.getError()+"\"}",HttpStatus.valueOf(e.getCode()));
         }catch (Exception e){
             logger.error("error al realizar la carga mexico ::"+e.getMessage(),e);
-            return new ResponseEntity<Object>("{\"error\":\"Servicio no disponible.\"}",HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Object>("{\"error\":\"Servicio no disponible para carga.\"}",HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
